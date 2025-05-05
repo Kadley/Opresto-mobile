64bb4b6b119dc8cd6098aacd8604d074e7d9a57f
Quentin Logie <loquente62@gmail.com>
2025-05-05 10:31:32 +0200

    Correction Étape 3 : Mutation créer / modifier une ville

--- src/graphql/city/resolver.ts
+++ src/graphql/city/resolver.ts
@@ -7,6 +7,25 @@ const cityResolver: Resolvers<Context> = {
       return ctx.dataSources.prisma.city.findMany();
     },
   },
+  Mutation: {
+    async createCity(_parent, args, ctx) {
+      const city = await ctx.dataSources.prisma.city.create({
+        data: args.input,
+      });
+
+      return city;
+    },
+    async updateCity(_parent, args, ctx) {
+      const city = await ctx.dataSources.prisma.city.update({
+        where: {
+          id: args.id,
+        },
+        data: args.input,
+      });
+
+      return city;
+    },
+  },
 };
 
 export default cityResolver;
